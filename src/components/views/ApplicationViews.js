1021568fed87039c19a5baf83ce3d921a5c24b4c
Daniel Myers <danielm53861991@gmail.com>
2023-06-12 09:59:04 -0500

    made some changes

---
+++ src/components/views/ApplicationViews.js
@@ -0,0 +1,7 @@
+export const ApplicationViews = () => {
+	return <>
+		<h1 className="title--main">Creative Writing Website</h1>
+		<div>Your one-stop shop for repairing your writing</div>
+	</>
+}
+
