4af505c70875dcea7f9ed6be09ab797a05c5e6a4
Daniel Myers <danielm53861991@gmail.com>
2023-06-30 12:24:27 -0500

    the site is working. Now I need to work on some css.

--- src/components/API/API.js
+++ src/components/API/API.js
@@ -4,42 +4,81 @@ export const API = () => {
   const [author, setAuthor] = useState("");
   const [authorBooks, setAuthorBooks] = useState([]);
 
-  const fetchAuthor = () => {
-    const apiUrl = `https://openlibrary.org/search.json?author=${encodeURIComponent(author)}`;
+  const fetchAuthorBooks = () => {
+    const apiUrl = `https://openlibrary.org/search.json?author=${encodeURIComponent(
+      `"${author}"`
+    )}`;//the quotes around author ensure that we get the exact match. 
     fetch(apiUrl)
       .then(response => response.json())
       .then(data => {
         const books = data.docs.map(doc => ({
           title: doc.title,
           firstLine: doc.first_sentence,
+          publicationYear: doc.first_publish_year,
+          pageNumber: doc.number_of_pages_median,
+          authorName: doc.author_name,
+          coverId: doc.cover_i // OLID of the book cover
         }));
         setAuthorBooks(books);
       })
       .catch(error => {
-        console.error("Error fetching author data:", error);
+        console.error("Error fetching author book data:", error);
       });
   };
 
   useEffect(() => {
     if (author !== "") {
-      fetchAuthor();
+      fetchAuthorBooks();
+    } else {
+      setAuthorBooks([]);
     }
   }, [author]);
+
+  const handleInputChange = e => {
+    setAuthor(e.target.value);
+    setAuthorBooks([]);
+  };
+
+  const formatAuthors = authors => {
+    if (authors.length === 1) {
+      return authors[0];
+    } else if (authors.length === 2) {
+      return authors.join(" and ");
+    } else {
+      const lastAuthor = authors[authors.length - 1];
+      const otherAuthors = authors.slice(0, authors.length - 1);
+      return `${otherAuthors.join(", ")}, and ${lastAuthor}`;
+    }
+  };
+
+  const getBookCoverUrl = coverId => {
+    if (coverId) {
+      return `https://covers.openlibrary.org/b/id/${coverId}-L.jpg`;
+    }
+    return ""; // Return an empty string if coverId is not available
+  };
 
   return (
     <div>
       <input
         type="text"
         value={author}
-        onChange={e => setAuthor(e.target.value)}
+        onChange={handleInputChange}
         placeholder="Enter author name"
       />
       {authorBooks.length > 0 ? (
         <article>
           {authorBooks.map((book, index) => (
             <div key={index}>
-              <h3>{book.title}</h3>
-              <p>{book.firstLine}</p>
+              <div>{book.coverId && (
+                <img src={getBookCoverUrl(book.coverId)} alt="Book Cover" />
+              )}</div>
+              <h2>Title: {book.title}</h2>
+              <h3>Author: {formatAuthors(book.authorName)}</h3>
+              <h4>Pages: {book.pageNumber}</h4>
+              <h4>Publication Date: {book.publicationYear}</h4>
+              {book.firstLine && <p>First Line: {book.firstLine}</p>}
+              
             </div>
           ))}
         </article>
